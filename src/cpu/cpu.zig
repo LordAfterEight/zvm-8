367395b1872fda5ae5f135b82689e88b38e5a660
LordAfterEight <elias@stettmayer.de>
2026-03-19 20:10:48 +0100

    Implement CPU reset logic

--- src/cpu/cpu.zig
+++ src/cpu/cpu.zig
@@ -3,6 +3,8 @@ const zvm = @import("../root.zig");
 pub const CPU = struct {
     /// General Purpose Register, 8-bit
     gpr: [32]zvm.core.reg.Reg8,
+    /// Instruction Register, 8-bit
+    ir: zvm.core.reg.Reg8,
     /// Instruction Pointer Register, 32-bit
     ipr: zvm.core.reg.Reg32,
     /// Stack Pointer Register, 32-bit
@@ -17,6 +19,7 @@ pub const CPU = struct {
     pub fn init() CPU {
         var cpu = CPU {
             .gpr = [_]zvm.core.reg.Reg8{.new("GPR")} ** 32,
+            .ir = zvm.core.reg.Reg8.new("Instruction Register"),
             .ipr = zvm.core.reg.Reg32.new("Instruction Pointer"),
             .spr = zvm.core.reg.Reg32.new("Stack Pointer"),
             .alu = zvm.core.alu.ALU.init(),
@@ -34,6 +37,11 @@ pub const CPU = struct {
         self.bus = bus;
     }
 
+    /// Advances the IPR, wrapping
+    pub fn step(self: *CPU) void {
+        self.ipr.load_uint(@as(u32, @intCast(self.ipr.get_value() + 1)));
+    }
+
     /// Loads a byte from the given address
     pub fn load(self: *CPU, address: usize) u8 {
         return self.bus.load_u8(address);
@@ -44,9 +52,31 @@ pub const CPU = struct {
         self.bus.store_u8(address, value);
     }
 
+    /// Jumps to the reset vector, gets a new address to continue from and jumps there.
+    /// Clobbers GPRs 0-3
+    pub fn reset(self: *CPU) void {
+        self.ipr.load_uint(@as(u32, 0xFFFFFC));
+        self.gpr[0].load_uint(self.bus.load_u8(self.ipr.get_value()));
+        self.step();
+        self.gpr[1].load_uint(self.bus.load_u8(self.ipr.get_value()));
+        self.step();
+        self.gpr[2].load_uint(self.bus.load_u8(self.ipr.get_value()));
+        self.step();
+        self.gpr[3].load_uint(self.bus.load_u8(self.ipr.get_value()));
+        self.ipr.load_uint(@as(u32, @intCast(self.gpr[0].get_value() << 24 | self.gpr[1].get_value() << 16 | self.gpr[2].get_value() << 8 | self.gpr[3].get_value())));
+    }
+
     /// Executes one full cycle
     pub fn tick(self: *CPU) anyerror!void {
-        _ = self;
+        self.reset();
+        self.ir.load_uint(self.bus.load_u8(self.ipr.get_value()));
         return;
+    }
+
+    /// Starts the CPU
+    pub fn run(self: *CPU) void {
+        while (1) {
+            _ = self.tick();
+        }
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -5,19 +5,14 @@ pub fn main() anyerror!void {
     try zvm.timer.start();
     var cpu = zvm.core.cpu.CPU.init();
     var bus = zvm.mem.bus.Bus.init();
-    var flash = zvm.components.flash.Flash.init();
-
-
     cpu.connect_bus(&bus);
 
-    const flash_device = zvm.device.device.Device.create(&flash, "Storage");
-    bus.map_device(0x10000, 0x8000, flash_device);
+    bus.ram[0xFFFFFC] = 0x1A;
+    bus.ram[0xFFFFFD] = 0x1B;
+    bus.ram[0xFFFFFE] = 0x1C;
+    bus.ram[0xFFFFFF] = 0x1D;
 
-    cpu.ipr.load_uint(@as(u32, 0xFFFFFFC));
+    cpu.reset();
 
-    const addr = 0x10010;
-
-    cpu.store(addr, 253);
-    const val = cpu.load(addr);
-    std.debug.print("RAM value at 0x{X:07}: {d}", .{addr, val});
+    std.debug.print("IPR: {X}", .{cpu.ipr.get_value()});
 }
